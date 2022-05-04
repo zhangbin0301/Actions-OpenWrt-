f3ac9cb20b1941b6787ab01baa8e7130a4e1a107
zhangbin0301 <zhangbin0301@gmail.com>
2022-05-04 10:22:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git OpenAppFilter git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter' >>feeds.conf.default
+# echo 'src-git OpenAppFilter git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter' >>feeds.conf.default
